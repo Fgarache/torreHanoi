5c0a685c79bc8fb7c5410645d3254e352471c65f
Francisco Garache <franciscogarach3@gmail.com>
2022-05-03 15:23:57 -0600

    54t

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,18 @@ public class Main {
     }
 
     public static void Hanoi(int n, int origen,  int auxiliar, int destino){
+
         if(n==1){
             System.out.println("mover disco de " + origen + " a " + destino);
         }else{
             Hanoi(n-1, origen, destino, auxiliar);
             System.out.println("mover disco de "+ origen + " a " + destino);
             Hanoi(n-1, auxiliar, origen, destino);
+
+
+
         }
+
     }
+
 }
